c82fe6c4af03fddaf2f74dd5a55beca0543b154c
frankradio123 <101906330+frankradio123@users.noreply.github.com>
2022-03-18 23:32:40 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+var prodsObj =
+[{
+  "_id": {
+    "$oid": "61e26a1c3e3d9208ec813686"
+  },
+  "video": [],
+  "image": [
+    "https://t3.ftcdn.net/jpg/02/61/85/92/360_F_261859293_WvBhDNgsf1H58DCUcrAyHqPXvoEu8cGV.jpg","https://rotulosdecorativos.com/wp-content/uploads/2018/11/S001019.jpg"
+  ],
+  "titulo": "TESP",
+  "precio": "12",
+  "body": "sssI",
+  "modelo": "12",
+  "peso": "3kg",
+  "dimensiones": "22x22",
+  "html":"<table><thead><tr><th>Características</th><th>Equipo Actual</th></tr></thead><tbody><tr><td>Portable</td><td style='color:green'>✔</td></tr><tr><td>Potencia de salida</td><td>incomparable!</td></tr><tr><td>Diseño versátil de aplicación práctica</td><td style='color:green'>✔</td></tr><tr><td>Manual de uso</td><td style='color:green'>✔</td></tr></tbody></table>",
+  "created": {
+    "$date": "2022-01-15T06:30:52.373Z"
+  },
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "61e26a1c3e3d9208ec813686"
+  },
+  "video": [
+    "https://ak.picdn.net/shutterstock/videos/30996259/preview/stock-footage-abstract-ink-color-splash-mix-in-water.webm"
+  ],
+  "image": [
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lamborghini-sian-roadster-1594225634.jpg?crop=1.00xw:0.892xh;0,0.0446xh&resize=1200:*"
+  ],
+  "titulo": "ATESP",
+  "precio": "123",
+  "body": "ssssssssssssssssssssssssssssssssssI",
+  "modelo": "12",
+  "peso": "3kg",
+  "dimensiones": "22x22",
+  "created": {
+    "$date": "2022-01-15T06:30:52.373Z"
+  },
+  "__v": 0
+}]
+// -----------
+var externosObj = {
+  "modo": "https://api.whatsapp.com/send?phone=",
+  "numero": "091900333",
+  "donar_url": "test.com",
+  "mail": "vivenus@mail.com",
+  "media_link": "youtube.com",
+  "social_link": "s//n",
+  "numero_apped": "59895330453"
+}
