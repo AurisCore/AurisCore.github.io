5c523815adaf08990fdb0f261cc3246f1509c74f
AurisCore <101906330+AurisCore@users.noreply.github.com>
2022-03-29 20:36:20 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,21 @@
 var prodsObj =
 [{
-  "_id": {
-    "$oid": "61e26a1c3e3d9208ec813686"
-  },
-  "video": [],
-  "image": [
-    "https://cdn.getfpv.com/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/h/d/hdzero_happymodel_mobula7hdzero-mobula7-low_res-width-1000px.jpg"
-  ],
   "titulo": "MAGA-ZINE",
   "precio": "600",
   "currency": "USD",
+  
+  "video": [],
+  "thumb_image":"https://cdn.getfpv.com/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/t/b/tbs-crossfire-nano-rx-pro-main-front.jpg",
+  "image": [
+    "https://sc04.alicdn.com/kf/H7a2d8689d3514794b2780a5aafe417bdm.jpg"
+  ],
   "body": "MAGAZINE ALM _SS",
   "modelo": "12",
   "peso": "3kg",
   "dimensiones": "22x22",
   "html":"<table><thead><tr><th>Características</th><th>Equipo Actual</th></tr></thead><tbody><tr><td>Portable</td><td style='color:green'>✔</td></tr><tr><td>Potencia de salida</td><td>incomparable!</td></tr><tr><td>Diseño versátil de aplicación práctica</td><td style='color:green'>✔</td></tr><tr><td>Manual de uso</td><td style='color:green'>✔</td></tr></tbody></table>"
-},
-{
-  "video": [],
-  "image": [
-    "https://cdn.getfpv.com/media/catalog/product/cache/1/small_image/200x200/9df78eab33525d08d6e5fb8d27136e95/f/l/flywoo-cinerace20-v1.2-neon-led-2-4s-analog-pro-cinewhoop-w-caddx-baby-ratel-v2-main-2.jpg"
-  ],
-  "titulo": "AVALSOX",
-  "precio": "800",
-  "currency": "USD",
-  "body": "",
-  "modelo": "11",
-  "peso": "33kg",
-  "dimensiones": "22x22",
-  "html":"<table><thead><tr><th>Características</th><th>Equipo Actual</th></tr></thead><tbody><tr><td>Portable</td><td style='color:green'>✔</td></tr><tr><td>Potencia de salida</td><td>incomparable!</td></tr><tr><td>Diseño versátil de aplicación práctica</td><td style='color:green'>✔</td></tr><tr><td>Manual de uso</td><td style='color:green'>✔</td></tr></tbody></table>"
-}]
+}
+]
 // -----------
 var externosObj = {
   "modo": "https://api.whatsapp.com/send?phone=",
