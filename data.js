185aa0f7517974ee73d74ca2a10d803ae5898428
AurisCore <101906330+AurisCore@users.noreply.github.com>
2022-03-29 19:51:58 +0200

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ var prodsObj =
   },
   "video": [],
   "image": [
-    "https://t3.ftcdn.net/jpg/02/61/85/92/360_F_261859293_WvBhDNgsf1H58DCUcrAyHqPXvoEu8cGV.jpg","https://rotulosdecorativos.com/wp-content/uploads/2018/11/S001019.jpg"
+    "https://cdn.getfpv.com/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/h/d/hdzero_happymodel_mobula7hdzero-mobula7-low_res-width-1000px.jpg"
   ],
   "titulo": "MAGA-ZINE",
   "precio": "600",
@@ -17,25 +17,6 @@ var prodsObj =
   "html":"<table><thead><tr><th>Características</th><th>Equipo Actual</th></tr></thead><tbody><tr><td>Portable</td><td style='color:green'>✔</td></tr><tr><td>Potencia de salida</td><td>incomparable!</td></tr><tr><td>Diseño versátil de aplicación práctica</td><td style='color:green'>✔</td></tr><tr><td>Manual de uso</td><td style='color:green'>✔</td></tr></tbody></table>"
 },
 {
-  "_id": {
-    "$oid": "61e26a1c3e3d9208ec813686"
-  },
-  "video": [
-    "https://ak.picdn.net/shutterstock/videos/30996259/preview/stock-footage-abstract-ink-color-splash-mix-in-water.webm"
-  ],
-  "image": [
-    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lamborghini-sian-roadster-1594225634.jpg?crop=1.00xw:0.892xh;0,0.0446xh&resize=1200:*"
-  ],
-  "titulo": "ATESP",
-  "precio": "123",
-  "body": "ssssssssssssssssssssssssssssssssssI",
-  "modelo": "12",
-  "peso": "3kg",
-  "dimensiones": "22x22",
-  "created": {
-    "$date": "2022-01-15T06:30:52.373Z"
-  },
-  "__v": 0
 }]
 // -----------
 var externosObj = {
