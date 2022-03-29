ff22a4b950144b56c5e913df9a94c87e646caeb3
AurisCore <101906330+AurisCore@users.noreply.github.com>
2022-03-29 20:26:25 +0200

    Update data.js

--- data.js
+++ data.js
@@ -17,6 +17,18 @@ var prodsObj =
   "html":"<table><thead><tr><th>Características</th><th>Equipo Actual</th></tr></thead><tbody><tr><td>Portable</td><td style='color:green'>✔</td></tr><tr><td>Potencia de salida</td><td>incomparable!</td></tr><tr><td>Diseño versátil de aplicación práctica</td><td style='color:green'>✔</td></tr><tr><td>Manual de uso</td><td style='color:green'>✔</td></tr></tbody></table>"
 },
 {
+  "video": [],
+  "image": [
+    "https://cdn.getfpv.com/media/catalog/product/cache/1/small_image/200x200/9df78eab33525d08d6e5fb8d27136e95/f/l/flywoo-cinerace20-v1.2-neon-led-2-4s-analog-pro-cinewhoop-w-caddx-baby-ratel-v2-main-2.jpg"
+  ],
+  "titulo": "AVALSOX",
+  "precio": "800",
+  "currency": "USD",
+  "body": "",
+  "modelo": "11",
+  "peso": "33kg",
+  "dimensiones": "22x22",
+  "html":"<table><thead><tr><th>Características</th><th>Equipo Actual</th></tr></thead><tbody><tr><td>Portable</td><td style='color:green'>✔</td></tr><tr><td>Potencia de salida</td><td>incomparable!</td></tr><tr><td>Diseño versátil de aplicación práctica</td><td style='color:green'>✔</td></tr><tr><td>Manual de uso</td><td style='color:green'>✔</td></tr></tbody></table>"
 }]
 // -----------
 var externosObj = {
