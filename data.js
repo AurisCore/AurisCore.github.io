15450af14b09905fe37adafe0b3f1faec26ba07a
AurisCore <101906330+AurisCore@users.noreply.github.com>
2022-03-30 17:39:08 +0200

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,8 @@ var prodsObj =
   "video": [],
   "thumb_image":"https://cdn.getfpv.com/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/t/b/tbs-crossfire-nano-rx-pro-main-front.jpg",
   "image": [
-    "https://sc04.alicdn.com/kf/H7a2d8689d3514794b2780a5aafe417bdm.jpg"
+    "https://sc04.alicdn.com/kf/H7a2d8689d3514794b2780a5aafe417bdm.jpg",
+    "https://www.robotec.com.uy/api/PublicTempStorage/multimedia/img5s_27376499_35a060be3b1245f29f3c550bda3706b3.jpg"
   ],
   "body": 'Este nuevo set básico ("core set") está optimizado para su uso educativo en clase o en casa y contiene todo lo que necesita para aprender con el estimulante set LEGO ® MINDSTORMS ®. Permite a los estudiantes construir, programar y probar sus soluciones basadas en la tecnología robótica de vida real.  Contiene el ladrillo inteligente EV3, un pequeño pero potente ordenador que hace posible el control de servo-motores y captar información de sensores. También permite la comunicación BT y WiFi, así como la programación y registro de datos ("data logging").',
   "modelo": "12",
