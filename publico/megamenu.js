18954a60e178044593d00f5ab8c6781efeec4430
AurisCore <101906330+AurisCore@users.noreply.github.com>
2022-05-11 23:44:39 +0200

    Update megamenu.js

--- publico/megamenu.js
+++ publico/megamenu.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
 
 
-    $(".menu-container").before("<a href=\"#\" class=\"menu-mobile\"></a>");
+    $(".menu-container").after("<a href=\"#\" class=\"menu-mobile\"></a>");
 
     //Adds menu-mobile class (for mobile toggle menu) before the normal menu
     //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
